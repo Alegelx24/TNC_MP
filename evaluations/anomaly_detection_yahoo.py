5941092328b5688c1e9328aa7bc594bd93c0e559
Alessandro Gelsi <79928745+Alegelx24@users.noreply.github.com>
2023-12-09 18:25:28 +0100

    working anomaly detection script for yahoo

--- evaluations/anomaly_detection_yahoo.py
+++ evaluations/anomaly_detection_yahoo.py
@@ -3,14 +3,13 @@ import os
 import pickle
 import numpy as np
 import random
-
 from tnc.models import RnnEncoder 
 from sklearn.metrics import roc_auc_score, average_precision_score
 from sklearn.utils import column_or_1d
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-encoder = RnnEncoder(encoding_size=10, hidden_size=100, in_channel=1)
+encoder = RnnEncoder(hidden_size=100, in_channel=1, encoding_size=10, device=device)
 tcl_checkpoint = torch.load('./ckpt/yahoo/checkpoint_0.pth.tar')
 # tcl_checkpoint = torch.load('./ckpt/waveform_trip/checkpoint.pth.tar')
 encoder.load_state_dict(tcl_checkpoint['encoder_state_dict'])
@@ -20,49 +19,41 @@ encoder.to(device)
 window_size = 30
 path = './data/yahoo_data/'
 
-
 with open(os.path.join(path, 'yahoo_x_test.pkl'), 'rb') as f:
     x_test = pickle.load(f)
 with open(os.path.join(path, 'yahoo_y_test.pkl'), 'rb') as f:
     y_test = pickle.load(f)
 
-# with open(os.path.join(path, 'x_train.pkl'), 'rb') as f:
-#     x_train = pickle.load(f)
-# with open(os.path.join(path, 'state_train.pkl'), 'rb') as f:
-#     y_train = pickle.load(f)
+x_test=torch.Tensor(x_test)
+y_test=torch.Tensor(y_test)
 
-T = torch.Tensor(x_test).shape[-1]
-x_window = np.split(torch.Tensor(x_test)[ :window_size * (T // window_size)], (T // window_size), -1)
+T = x_test.shape[-1]
+
+x_test = x_test.unsqueeze(1)
+
+x_window = np.split(torch.Tensor(x_test)[ : ,:,:window_size * (T // window_size)], (T // window_size), -1)
 x_window = np.concatenate(x_window, 0)
-y_window = np.concatenate(np.split(torch.Tensor(y_test)[:, :window_size * (T // window_size)], (T // window_size), -1),0).astype(int)
-y_window = np.array([np.bincount(yy).argmax() for yy in y_window])
 
-T = torch.Tensor(x_test).shape[-1]
-# x_window_train = np.split(x_train[:, :, :window_size * (T // window_size)], (T // window_size), -1)
-# x_window_train = np.concatenate(x_window_train, 0)
-# y_window_train = np.concatenate(np.split(y_train[:, :window_size * (T // window_size)], (T // window_size), -1),0).astype(int)
-# y_window_train = np.array([np.bincount(yy).argmax() for yy in y_window_train])
-# shuffled_inds_train = list(range(len(x_window_train)))
-# random.shuffle(shuffled_inds_train)
+#simple splitting in window length sequence 
+y_window = np.concatenate(np.split((y_test)[:, :window_size * (T // window_size)], (T // window_size), -1),0).astype(int) 
+#y_window = np.array([np.bincount(yy).argmax() for yy in y_window])
+
+#all sequence is considered as a single sample
+y_window = np.array([1 if np.any(yy == 1) else 0 for yy in y_window])
+
 shuffled_inds_test = list(range(len(x_window)))
 random.shuffle(shuffled_inds_test)
 
-# print(x_window.shape, y_window.shape)
 testset = torch.utils.data.TensorDataset(torch.Tensor(x_window), torch.Tensor(y_window))
 test_loader = torch.utils.data.DataLoader(testset, batch_size=10, shuffle=True)
-# trainset = torch.utils.data.TensorDataset(torch.Tensor(x_window_train), torch.Tensor(y_window_train))
-# train_loader = torch.utils.data.DataLoader(trainset, batch_size=100, shuffle=True)
-
 
 is_anomaly = y_window.copy()
-is_anomaly = np.logical_or(is_anomaly==1, is_anomaly==2).astype(int)
 
+#Encoding phase
 encodings_train = []
-############################################################################################ iNTERVENIRE QUI!!!!!!!!!
 for x,_ in test_loader:
     encodings_train.append(encoder(x.to(device)).detach().cpu().numpy())
 encodings_train = np.concatenate(encodings_train, 0)
-print(encodings_train.shape)
 
 # train the KNN detector
 from pyod.models.knn import KNN
@@ -70,7 +61,7 @@ from pyod.models.pca import PCA
 from pyod.models.lof import LOF
 from pyod.models.cblof import CBLOF
 from pyod.models.mcd import MCD
-from pyod.models.lscp import LSCP
+#from pyod.models.lscp import LSCP
 # from pyod.models.auto_encoder import AutoEncoder
 
 clf_knn = KNN()
@@ -97,7 +88,6 @@ anomaly_scores_cblof = clf_cblof.decision_function(encodings_train)
 # anomaly_scores_lscp = clf_lscp.decision_function(encodings_train)
 # anomaly_scores_ae = clf_ae.predict_proba(encodings_train)
 
-
 # y_test_scores = []
 # for x,_ in test_loader:
 #     encodings_test = encoder(torch.Tensor(x).to(device))
@@ -105,8 +95,8 @@ anomaly_scores_cblof = clf_cblof.decision_function(encodings_train)
 #     y_test_scores.extend(probs[:,0])
 # y_test_scores = np.array(y_test_scores)
 
+y_ind_0 = np.argwhere(y_window.reshape(-1, ) == 0)
 y_ind_1 = np.argwhere(y_window.reshape(-1, ) == 1)
-y_ind_3 = np.argwhere(y_window.reshape(-1, ) == 3)
 
 for i, anomaly_scores in enumerate([anomaly_scores_knn, anomaly_scores_lof, anomaly_scores_cblof, anomaly_scores_mcd, anomaly_scores_pca]):
     method = ['KNN', 'LOF', 'CBLOF', 'MCD', 'PCA'][i]
@@ -115,9 +105,9 @@ for i, anomaly_scores in enumerate([anomaly_scores_knn, anomaly_scores_lof, anom
     auprc = average_precision_score(column_or_1d(is_anomaly), column_or_1d(anomaly_scores))#[:,0])
     print('Anomaly detection AUC: ', auc)
     print('Anomaly detection AUPRC: ', auprc)
+    print('Label 0: ', np.mean(anomaly_scores[y_ind_0.reshape(-1,)]), '+-',
+          np.std(anomaly_scores[y_ind_0.reshape(-1,)]))
     print('Label 1: ', np.mean(anomaly_scores[y_ind_1.reshape(-1,)]), '+-',
           np.std(anomaly_scores[y_ind_1.reshape(-1,)]))
-    print('Label 3: ', np.mean(anomaly_scores[y_ind_3.reshape(-1,)]), '+-',
-          np.std(anomaly_scores[y_ind_3.reshape(-1,)]))
 
 
